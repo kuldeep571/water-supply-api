a46efcde9043d9b8af7b620f25514f7834437681
Rehan-Multani <mrmmultani@gmail.com>
2024-01-03 14:49:52 +0500

    2
    2

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1/water");
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log("Database Connected Successfully");
   } catch (error) {
     console.error("Database connection error:", error.message);
